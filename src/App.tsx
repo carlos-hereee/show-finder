512d3b93ed58ab1d1779ec841179a628f05bdd4e
ch04937 <97hernandez.c@gmail.com>
2020-12-14 21:06:18 -0600

    update file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import Header from "./pages/Header";
+import Main from "./pages/Main";
+import Footer from "./pages/Footer";
 import "./stylesheets/index.scss";
 
 const App: React.FC = () => (
